4ee722c3b9ffa8d7a605cf9c9f27f12d92ea19f0
Mark O'Neill <mto@byu.edu>
2015-03-13 17:12:05 -0600

    Various fixes to previous commit.  still working on others

--- secure_handshake_parser.c
+++ secure_handshake_parser.c
@@ -30,7 +30,7 @@ void th_read_request(pid_t pid, int sockfd, char* buf, long ret) {
 	}
 	memcpy(conn_state->send_buf + conn_state->send_buf_length, buf, ret);
 	conn_state->send_buf_length += ret;
-	while (conn_state->state != IRRELEVANT && conn_state->state != TLS_SERVER_HELLO && conn_state->send_buf_length >= conn_state->send_bytes_to_read) {
+	while (conn_state->state != IRRELEVANT && conn_state->state != TLS_SERVER_UNKNOWN && conn_state->send_buf_length >= conn_state->send_bytes_to_read) {
 		update_send_state(conn_state);
 	}
 	if (conn_state->state == IRRELEVANT) {
@@ -50,12 +50,19 @@ void th_read_response(pid_t pid, int sockfd, char* buf, long ret) {
 		th_conn_state_delete(pid, sockfd);
 		return;
 	}
+	printk(KERN_ALERT "loader says ret is %ld", ret);
+
+	printk(KERN_ALERT "loader says recv_buf_length is %u", conn_state->recv_buf_length);
         if ((conn_state->recv_buf = krealloc(conn_state->recv_buf, conn_state->recv_buf_length + ret, GFP_KERNEL)) == NULL) {
                 printk(KERN_ALERT "Oh noes!  krealloc failed!");
                 return;
         }
         memcpy(conn_state->recv_buf + conn_state->recv_buf_length, buf, ret);
         conn_state->recv_buf_length += ret;
+
+
+	printk(KERN_ALERT "afterward loader says ret is %ld", ret);
+
         while (conn_state->state != IRRELEVANT && conn_state->recv_buf_length >= conn_state->recv_bytes_to_read) {
                 update_recv_state(conn_state);
         }
@@ -82,6 +89,7 @@ void update_send_state(conn_state_t* conn_state) {
 			else {
 				conn_state->state = IRRELEVANT;
 			}
+			printk(KERN_ALERT "send buf length is %u", conn_state->send_buf_length);
 			break;
 		case TLS_CLIENT_NEW:
 			cs_buf = conn_state->send_buf;
@@ -102,7 +110,7 @@ void update_send_state(conn_state_t* conn_state) {
 			// Should never get here
 			break;
 		default:
-			printk(KERN_ALERT "Unknown connection state!");
+			//printk(KERN_ALERT "Unknown connection state!");
 			break;
 	}
 	return;
@@ -117,13 +125,15 @@ void update_recv_state(conn_state_t* conn_state) {
 	switch(conn_state->state) {
 		case TLS_SERVER_UNKNOWN:
                         if (conn_state->recv_buf[0] == TH_TLS_HANDSHAKE_IDENTIFIER) {
-                                print_call_info(sockfd, "may be doing SSL");
+                                print_call_info(sockfd, "remote may be doing SSL");
                                 conn_state->state = TLS_SERVER_NEW;
                                 conn_state->recv_bytes_to_read = TH_TLS_RECORD_HEADER_SIZE;
                         }
                         else {
                                 conn_state->state = IRRELEVANT;
                         }
+			printk(KERN_ALERT "recv buf length is %u and recvb2r is: %d", conn_state->recv_buf_length, conn_state->recv_bytes_to_read);
+			conn_state->state = IRRELEVANT;
 			break;
 		case TLS_SERVER_NEW:
                         cs_buf = conn_state->recv_buf;
@@ -138,6 +148,11 @@ void update_recv_state(conn_state_t* conn_state) {
 		case TLS_SERVER_HELLO:
 			conn_state->state = IRRELEVANT;
 			print_call_info(sockfd, "read all of SERVER_HELLO");
+			break;
+		case TLS_CLIENT_UNKNOWN: // temporarily just ignore connections made asyncronously
+		case TLS_CLIENT_NEW:
+		case TLS_CLIENT_HELLO:
+			conn_state->state = IRRELEVANT;
 			break;
 		case TLS_SERVER_CERTIFICATE:
 		case TLS_ESTABLISHED:
