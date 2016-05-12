3f7cfd8b51bfef35d8d7b63026747acd205bcf53
Jordan Whitehead <jordan9001@gmail.com>
2016-05-11 18:28:12 -0600

    Fixed a small bug

--- policy-engine/th_logging.h
+++ policy-engine/th_logging.h
@@ -1,6 +1,8 @@
 #ifndef TH_LOGGING_H_
 #define TH_LOGGING_H_
 
+#include <openssl/x509.h>
+
 typedef enum thlog_level_t {LOG_DEBUG=0, LOG_INFO=1, LOG_WARNING=2, LOG_ERROR=3, LOG_NONE=4} thlog_level_t;
 
 int thlog_init(const char *log_file_name, thlog_level_t min_level);
